527e00c282a48cace6d72bab419b63995e2af5f0
Dustin Baldwin <dustin@dustinbaldwin.com>
2015-12-21 14:44:45 -0500

    Remove restriction on headers in code

--- src/server.js
+++ src/server.js
@@ -22,8 +22,7 @@ var ajaxtest = function (app, router) {
 	 */
 	app.all('*', function (req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "X-Requested-With");
-		res.header('Access-Control-Allow-Headers', 'Content-Type');
+		
 		next();
 	});
 
